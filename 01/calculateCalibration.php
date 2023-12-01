aedbe72c15b886216f79abc823989da7661a2863
Siufong <siufong.h@gmail.com>
2023-12-01 17:58:31 +0000

    first star of day 1

---
+++ 01/calculateCalibration.php
@@ -0,0 +1,65 @@
+<?php
+
+class calculateCalibration
+{
+    public function calculateCalibration($input)
+    {
+        // we do an array with the values and the empty values
+        $inputIntoArray = preg_split("/[\n]+/", $input);
+
+        // remove empty break line
+        $inputIntoArray = array_map('trim', $inputIntoArray);
+
+        $numbers = [];
+
+        foreach ($inputIntoArray as $string)
+        {
+            $strlength = strlen($string);
+            $indexes = [];
+            for ($index = 0;  $index < $strlength; $index++)
+            {
+                $char = substr($string, $index, 1);
+
+                if (is_numeric($char))
+                {
+                    $indexes[] = $index;
+                }
+            }
+            
+            $min = min($indexes);
+            $max = max($indexes);
+            
+            $firstDigit = substr($string, $min, 1);
+            $lastDigit = substr($string, $max, 1);
+
+            $numbers[] = $firstDigit.$lastDigit;
+        }
+
+        // first star
+        return array_sum($numbers);
+    }
+
+    public function getNumbersFromAString($string)
+    {
+        $strLenght = strlen($string);
+
+        $indexes = [];
+        for ($index = 0;  $index < $strLenght ; $index++)
+        {
+            $char = substr($string, $index, 1);
+    
+            if (is_numeric($char))
+            {
+                $indexes[] = $index;
+            }
+        }
+
+        $min = min($indexes);
+        $max = max($indexes);
+
+        $firstDigit = substr($string, $min, 1);
+        $lastDigit = substr($string, $max, 1);
+
+        return $firstDigit.$lastDigit;
+    }
+}
